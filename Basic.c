ce6261994318fe4fb3e04fd3622b35add2b576f0
GaneshMohite17 <115934100+GaneshMohite17@users.noreply.github.com>
2022-10-16 19:03:59 +0530

    Demo Program

---
+++ Basic.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  printf("\n Student Details\n\n");
+  printf("===============********=================\n");
+  printf("\n %3d %-10s %3d %3d %3d %3d %0.2f" ,101,"Nikhil",70,80,90,240,80.00 );
+  printf("\n %3d %-10s %3d %3d %3d %3d %0.2f" ,102,"Pratik",75,70,65,210,70.00);
+  printf("\n %3d %-10s %3d %3d %3d %3d %0.2f" ,103,"Rushi",75,65,85,225,75.00);
+  printf("\n\n===============********=================\n");
+  getch();
+  return 0;
+}
